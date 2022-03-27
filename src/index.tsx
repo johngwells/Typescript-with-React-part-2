7b034302c62eafd50a1e6626f3a86fb10372f8d0
John Wells <molekewl2017@gmail.com>
2022-03-26 17:13:26 -0700

    custom plugin using file algorithm to create urls to packages to download

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
   const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
+  // In terminal:  npm view react dist.tarball
+  // links to reacts latest (source code latest version) tgz file
+  // use unpkg to get source code of react & pass back to the code
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
@@ -21,13 +25,23 @@ const App = () => {
   const handleOnClick = async () => {
     if (!ref.current) return;
 
-    // transpile
-    const result = await ref.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015'
+    // transpile : before
+    // const result = await ref.current.transform(input, {
+    //   loader: 'jsx',
+    //   target: 'es2015'
+    // });
+
+    // transpile : now
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
     });
-    console.log(result);
-    setCode(result.code);
+
+    const { outputFiles } = result;
+    console.log(outputFiles[0].text)
+    setCode(outputFiles[0].text);
   };
   
   return (
